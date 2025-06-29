87e4f2fb4c097db2320211f1d2b2c0510d4e75f3
sunny5130 <sunny51305527.com>
2025-06-29 18:43:52 +0530

    first day of react

---
+++ src/User.jsx
@@ -0,0 +1,19 @@
+function Home(){
+    return(
+        <>
+        <h1>Home</h1>
+        </>
+    );
+}
+export default Home;
+
+export function Profile(){
+    return(<>
+    <h1>Profile</h1>
+    </>);
+}
+export function Address(){
+    return(<>
+    <h1>Address</h1>
+    </>);
+}
