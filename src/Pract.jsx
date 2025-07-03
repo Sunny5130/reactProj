9ea238318b29ee580f0663fc0d8924fcf8577c6c
sunny5130 <sunny51305527.com>
2025-07-03 23:48:57 +0530

    nn

--- src/Pract.jsx
+++ src/Pract.jsx
@@ -8,7 +8,7 @@ function Operation(){
     return(
         <>
         <h1>States in react js</h1>
-        <h1>{fruit}</h1>
+        <h1>fruits: {fruit}</h1>
     <button onClick={handlefruit}>change fruit</button>
         </>
     )
